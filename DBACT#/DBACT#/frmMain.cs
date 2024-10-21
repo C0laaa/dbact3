39ecc008f45c28e4033eb134bb031151d18867d8
C0laaa <rcramos619@gmail.com>
2024-10-21 17:02:32 +0800

    Update frmMain.cs

--- DBACT#/DBACT#/frmMain.cs
+++ DBACT#/DBACT#/frmMain.cs
@@ -5,17 +5,18 @@ using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DBACT_
+namespace _045_ACT3_RAMOS_ReyChrstian
 {
     public partial class frmMain : Form
     {
         string _username;
+        
         OleDbConnection _conn;
-
         public frmMain(string username, OleDbConnection conn)
         {
             InitializeComponent();
@@ -25,7 +26,7 @@ namespace DBACT_
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            this.Text = "Welcome! " + _username;
+            this.Text = "Welcome " + _username;
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
@@ -36,81 +37,54 @@ namespace DBACT_
             }
             else if (cboGender.SelectedIndex == -1)
             {
-                MessageBox.Show("Please Select your Gender", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Please Select A Gender", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
                 DataTable dt = new DataTable();
-                if (cboGender.SelectedIndex == 0)
-                {
-                    string query = "SELECT name as NAME, email as EMAIL, sex as SEX, address as ADDRESS FROM employee ";
-                    if (_conn.State != ConnectionState.Open)
-                    {
-                        _conn.Open();
-                    }
-                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, _conn);
-                    adapter.Fill(dt);
-
-                    _conn.Close();
-
-                    grdData.DataSource = dt;
-                }
-                if (cboGender.SelectedIndex == 1 || cboGender.SelectedIndex == 2)
-                {
-                    string query = "SELECT name as NAME, email as EMAIL, sex as SEX, address as ADDRESS FROM employee WHERE name like '%" + txtKeyword.Text + "%' and sex ='" + cboGender.Text + "' "; if (_conn.State != ConnectionState.Open)
-                    {
-                        _conn.Open();
-                    }
-                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, _conn);
-                    adapter.Fill(dt);
-
-                    _conn.Close();
-
-                    grdData.DataSource = dt;
-                }
-
-            }
-
-        }
-
-        private void txtKeyword_TextChanged(object sender, EventArgs e)
-        {
-            DataTable dt = new DataTable();
-
-            if (cboGender.SelectedIndex >= 0)
-            {
-                string query = "SELECT name as NAME, email as EMAIL, sex as SEX, address as ADDRESS FROM employee WHERE name like '%" + txtKeyword.Text + "%' and sex ='" + cboGender.Text + "' "; 
+                
+                string query = "select name as NAME, email as EMAIL,sex as SEX, address as ADDRESS from employee where name like'%" + txtKeyword.Text + "%' and sex = '" + cboGender.Text + "'";
+                
                 if (_conn.State != ConnectionState.Open)
                 {
                     _conn.Open();
                 }
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, _conn);
+                adapter.Fill(dt);
+                
+                grdData.DataSource = dt;
+
+                _conn.Close();
+            }
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+           
+                DataTable dt = new DataTable();
+
+                string query = "select name as NAME, email as EMAIL,sex as SEX, address as ADDRESS from employee where name like'%" + txtKeyword.Text + "%'";
+
+                if (_conn.State != ConnectionState.Open)
+                {
+                    _conn.Open();
+                }
+
                 OleDbDataAdapter adapter = new OleDbDataAdapter(query, _conn);
                 adapter.Fill(dt);
 
-                _conn.Close();
-
                 grdData.DataSource = dt;
-            }
 
-
+                _conn.Close();
+            
+      
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            frmAdd frm = new frmAdd(_conn);
-            frm.ShowDialog();
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            frmUpdate frm = new frmUpdate(_conn);
-            frm.ShowDialog();
-        }
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            frmDelete frm = new frmDelete(_conn);
-            frm.ShowDialog();
+          frmAdd frm = new frmAdd(_conn);
+            frm.Show(); 
         }
 
         private void cboGender_SelectedIndexChanged(object sender, EventArgs e)
@@ -132,7 +106,7 @@ namespace DBACT_
             }
             else if (cboGender.SelectedIndex == 1)
             {
-                string query = "SELECT name as NAME, email as EMAIL, sex as SEX, address as ADDRESS FROM employee WHERE  sex = 'Male'"; 
+                string query = "SELECT name as NAME, email as EMAIL, sex as SEX, address as ADDRESS FROM employee WHERE  sex = 'Male'";
                 if (_conn.State != ConnectionState.Open)
                 {
                     _conn.Open();
@@ -158,7 +132,18 @@ namespace DBACT_
 
                 grdData.DataSource = dt;
             }
+        }
 
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            frmEdit frm = new frmEdit(_conn);
+            frm.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            frmDelete frm = new frmDelete(_conn);
+            frm.ShowDialog();
         }
     }
 }
